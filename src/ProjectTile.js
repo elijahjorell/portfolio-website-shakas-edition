4b5b4c452d298e374a44616ac1e8a45c90e3806f
Elijah <42728154+elijahjorell@users.noreply.github.com>
2021-02-04 16:38:52 +1100

    Try format card

--- src/ProjectTile.js
+++ src/ProjectTile.js
@@ -1,5 +1,6 @@
 import { React, useState } from "react";
 import { Modal, Button, Card } from "react-bootstrap";
+import { LetsEat } from "./LetsEat.png";
 
 const ProjectTile = () => {
   const [show, setShow] = useState(false);
@@ -19,23 +20,21 @@ const ProjectTile = () => {
       </Button>
 
       <Modal show={show} onHide={handleClose}>
-        <Modal.Body>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+        <Modal.Body style={{ padding: "0", border: "none" }}>
+          <Card style={{ width: "100%", border: "none", height: "500px" }}>
+            <div className="dummy-image"/>
             <Card.Body>
               <Card.Title>Card Title</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
               </Card.Text>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
             </Card.Body>
           </Card>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
       </Modal>
     </div>
   );
